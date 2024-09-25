4d89da96d04a43e1c4408de0cfe16d48edd5366e
Dmitrii Kiselev <didroi@yandex.ru>
2024-09-25 21:31:53 +0200

    new_changes

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,14 @@
 import allure
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class BasePage:
-    base_url = 'https://www.sbazar.cz//'
+    base_url = 'https://www.sbazar.cz/'
     page_url = None
+    page_title = None
 
     def __init__(self, driver: WebDriver):
         self.driver = driver
@@ -14,6 +18,23 @@ class BasePage:
         if self.page_url:
             self.driver.get(f'{self.base_url}{self.page_url}')
         else:
-            raise NotImplementedError('Unable to open that page by url')
-        print(self.driver.title)
+            self.driver.get(self.base_url)
+
+    @allure.step('Check Title')
+    def is_title_correct(self):
+        return self.driver.title == self.page_title
+
+    @allure.step('Scroll the page')
+    def scroll_page(self, pixels=None, start=0):
+        if pixels:
+            self.driver.execute_script(f"window.scrollTo({start}, {pixels})")
+        else:
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+    def find(self, locator):
+        wait = WebDriverWait(self.driver, 10)
+        return wait.until(ec.presence_of_element_located(locator))
+
+    def click(self, locator):
+        self.find(locator).click()
 
